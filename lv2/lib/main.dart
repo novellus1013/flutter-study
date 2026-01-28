806aeb976cdefc85ffba2f758e9a79ca4dbc7edd
novellus1013 <noveluslab@proton.me>
2026-01-28 16:47:42 +0900

    #2.기본 UI 작업하기

---
+++ lv2/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:lv2/user/view/login_screen.dart';
+
+void main() {
+  runApp(_App());
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'NotoSans'),
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
+}
